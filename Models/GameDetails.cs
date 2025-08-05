171991348d1696b6bfad85af59cf6641bbd476be
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-08-05 16:59:53 +0100

    AddClientSideValidationToForm

--- Models/GameDetails.cs
+++ Models/GameDetails.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.UI.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+    [Required, Display(Name = "Game Name"), StringLength(50)]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Genre is a required field")]
     public string? GenreId { get; set; }
+    [Required, Range(1, 100)]
     public decimal Price { get; set; }
+    [Required]
     public DateOnly ReleaseDate { get; set; }
 }
